9b6e89f4f6764a500eb2205315f7f855ee451dd0
Meskeremhab <meski123habtom@gmail.com>
2024-07-02 10:22:54 +0200

    cdk-app

---
+++ app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import os
+
+import aws_cdk as cdk
+
+from cloud_solutions_architecture_group_assignment.cloud_solutions_architecture_group_assignment_stack import CloudSolutionsArchitectureGroupAssignmentStack
+
+
+app = cdk.App()
+CloudSolutionsArchitectureGroupAssignmentStack(app, "CloudSolutionsArchitectureGroupAssignmentStack",
+    # If you don't specify 'env', this stack will be environment-agnostic.
+    # Account/Region-dependent features and context lookups will not work,
+    # but a single synthesized template can be deployed anywhere.
+
+    # Uncomment the next line to specialize this stack for the AWS Account
+    # and Region that are implied by the current CLI configuration.
+
+    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+
+    # Uncomment the next line if you know exactly what Account and Region you
+    # want to deploy the stack to. */
+
+    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+
+    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+    )
+
+app.synth()
---
+++ tests/unit/test_cloud_solutions_architecture_group_assignment_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from cloud_solutions_architecture_group_assignment.cloud_solutions_architecture_group_assignment_stack import CloudSolutionsArchitectureGroupAssignmentStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in cloud_solutions_architecture_group_assignment/cloud_solutions_architecture_group_assignment_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = CloudSolutionsArchitectureGroupAssignmentStack(app, "cloud-solutions-architecture-group-assignment")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
